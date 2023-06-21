0276c256e84c36cabf93fbc5d3b5b4e2001c9ad6
Santiago Espinel <81175161+santiago038912@users.noreply.github.com>
2023-06-20 20:48:13 -0500

    unfinished oauth security system

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-from fastapi import FastAPI, Depends
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-
-from config.db import collection
+from fastapi import FastAPI
 
 from middleware.error_handler import ErrorHandler
 
-from typing import Annotated
-
 from routers.user import user_router
-
-from models.user import User
 
 # APP_INFO
 app = FastAPI()
@@ -19,11 +12,5 @@ app.version = "0.0.1"
 # MIDDLEWARE
 # app.add_middleware(ErrorHandler)
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
 # ROUTERS
 app.include_router(user_router)
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
-    return user
